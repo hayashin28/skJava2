86d0cd16755552b95bc819c3fc6bece18d317ae2
hayas <hayas@HAYASHIN-WIMMAX2>
2023-04-30 15:59:21 +0900

    Lesson06 完了

---
+++ src/skJava/lesson06/Sample06.java
@@ -0,0 +1,27 @@
+package skJava.lesson06;
+
+public class Sample06 {
+
+	public static void main(String[] args) {
+		
+		Car car1;
+		Car car2;
+		// インスタンス化
+		car1 = new Car();
+		car2 = new Car();
+		// フィールドに値を設定
+		car1.setNum(1185);
+		car1.setGas(1.6);
+		car2.setNumGas(1192, 65.3);
+
+		System.out.println("車両Noは" + car1.getNum() + "です。");
+		System.out.println("ガソリン量は" + car1.getGas() + "です。");
+
+		System.out.println();
+		
+		System.out.println("車両Noは" + car2.getNum() + "です。");
+		System.out.println("ガソリン量は" + car2.getGas() + "です。");
+
+		
+	}
+}
